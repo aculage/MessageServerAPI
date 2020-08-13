2501c6407cb94a9b65160706d80d431c6a53c1d8
arseniculage <worknavo@mail.ru>
2020-08-13 17:25:16 +0300

    Docker working. Final commit.

--- dbscript.sql
+++ dbscript.sql
@@ -1,22 +1,19 @@
-CREATE USER client WITH PASSWORD 'client';
-CREATE DATABASE mservapi_deb;
-GRANT ALL PRIVILEGES ON DATABASE mservapi_deb TO client;
-
+\c mservapi_deb
 CREATE TABLE users (
-    id uuid not null primary key,
-    username varchar not null unique,
-    creation_time timestamp
+                       id uuid not null primary key,
+                       username varchar not null unique,
+                       creation_time timestamp
 );
 CREATE TABLE chats (
-    id uuid not null primary key,
-    name varchar not null,
-    users uuid[] not null,
-    creation_time timestamp
+                       id uuid not null primary key,
+                       name varchar not null,
+                       users uuid[] not null,
+                       creation_time timestamp
 );
 CREATE TABLE messages (
-    id uuid  not null primary key,
-    chat uuid not null,
-    author uuid not null,
-    mtext TEXT not null,
-    creation_time timestamp
+                          id uuid  not null primary key,
+                          chat uuid not null,
+                          author uuid not null,
+                          mtext TEXT not null,
+                          creation_time timestamp
 );
